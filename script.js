43cf3a5c09318dcfc3e6a4f907ff3a1320ba7d1d
e2324hha <pompome2324@gmail.com>
2024-10-18 13:43:03 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     liff.init({
-        liffId: '2006463532-LVe3Gvzm', // Use own liffId
+        liffId: '2006463532-QGAMO5VJ', // Use own liffId
         withLoginOnExternalBrowser: true, // Enable automatic login process
     }).then(() => {
         // Start to use liff's api
     });
-    
+
     liff.init(() => {
         const idToken = liff.getDecodedIDToken();
         console.log(idToken); // print decoded idToken object
